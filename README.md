ee6c71d6130beda39ea37cf98d81f6e4e2f2f452
Igor Tarlinskiy <itarlinskiy@gmail.com>
2025-09-23 12:46:26 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+## 📦 Installation
+
+Make sure you have Python and Scrapy installed:
+
+```bash
+git clone https://github.com/xpatronum/jaiai && cd jaiai
+
+pip install -r requirements.txt
+```
+
+
+## 🕷️ Scrapping
+
+Welcome to the Scrapping Project! This project uses **Scrapy** to collect investment reviews from [banki.ru](https://www.banki.ru). Follow the steps below to get started. 🚀
+
+
+## 🏁 How to Run the Scrapy Spider
+
+To start scrapping all investment review pages, use the following command in your terminal:
+
+```bash
+scrapy crawl all_pages
+```
+
+Or, to scrape just one page:
+
+```bash
+scrapy crawl one_page
+```
+
+## 🌐 Example
+
+```bash
+scrapy crawl all_pages -o all_pages_sravni_result.json
+```
+
+This will save the results to `all_pages_sravni_result.json`.
+
+## 📂 Output
+
+Scraped data will be saved according to your spider's settings (e.g., JSON, CSV, or database).
+
+## 💡 Tips
+
+- Check your spider code for custom settings.
+- Use `-o output.json` to save results to a file.
+
+## 🛠️ Happy Scrapping!
+
+---
+
+### Powered by `justatom.org`
---
+++ jaiai/spiders/prime.py
@@ -0,0 +1,87 @@
+import http
+import os
+import re
+from pathlib import Path
+from urllib.parse import urljoin
+
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+from w3lib.html import remove_tags
+
+
+class OnePageSpider(CrawlSpider):
+    # name – имя паука
+    name = "one_page"
+    # allowed_domains – домены сайта, в пределах которого необходимо сканировать
+    allowed_domains = ["www.banki.ru"]
+    # start_urls – список начальных адресов
+    start_urls = ["https://www.banki.ru/investment/responses/list"]
+
+    # rules - правила, определяющие поведение паука
+    # первое правило: проваливаемся внутрь отзыва для того, чтобы достать заголовок и текст отзыва
+    rules = (
+        Rule(
+            LinkExtractor(
+                restrict_xpaths="//div[@class = 'responses__item__message']/a"
+            ),
+            callback="parse_item",
+            follow=True,
+        ),
+    )
+
+    def parse_item(self, response):
+        item = {}
+        item["title"] = (
+            response.xpath("//h1[contains(@class, 'response-page__title')]/text()")
+            .get()
+            .strip()
+        )
+        item["review"] = (
+            response.xpath("//div[contains(@class, 'article-text')]/text()")
+            .get()
+            .strip()
+        )
+        return item
+
+
+class AllPagesInvestmentSpider(CrawlSpider):
+    # name – имя паука
+    name = "all_pages"
+    # allowed_domains – домены сайта, в пределах которого необходимо сканировать
+    allowed_domains = ["www.banki.ru"]
+    # start_urls – список начальных адресов
+    start_urls = ["https://www.banki.ru/investment/responses/list"]
+    """
+    rules - правила, определяющие поведение паука
+    первое правило: проваливаемся внутрь отзыва для того, чтобы достать заголовок и текст отзыва
+    второе правило: переходим по страницам
+    """
+    rules = (
+        Rule(
+            LinkExtractor(
+                restrict_xpaths="//div[@class = 'responses__item__message']/a"
+            ),
+            callback="parse_item",
+            follow=True,
+        ),
+        Rule(
+            LinkExtractor(
+                restrict_xpaths="//li[@class='ui-pagination__item ui-pagination__next']/a"
+            )
+        ),
+    )
+
+    def parse_item(self, response):
+        item = {}
+        item["title"] = (
+            response.xpath("//h1[contains(@class, 'response-page__title')]/text()")
+            .get()
+            .strip()
+        )
+        item["review"] = (
+            response.xpath("//div[contains(@class, 'article-text')]//text()")
+            .get()
+            .strip()
+        )
+        return item
